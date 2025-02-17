e1ec3b29da7f1b70a9abc4dfe82bfc55a8fc90b6
Yulinovity44524 <shibojakukak@gmail.com>
2025-02-18 00:02:12 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,31 @@
-from telegram import ReplyKeyboardMarkup, Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
-# Токен вашего бота
-TOKEN = '7835506064:AAGyhtbS72oN0EyXfOI_HDNTaUtYb1pkrS8'
+def start(update: Update, context: CallbackContext) -> None:
+    """Обработчик команды /start, показывает меню выбора"""
+    keyboard = [
+        [InlineKeyboardButton("Pubg Mobile", callback_data='pubg_key')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Привет! Что хочешь выбрать:', reply_markup=reply_markup)
 
-# Функция для обработки команды /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Создаем клавиатуру с кнопками
-    keyboard = [['Кнопка 1', 'Кнопка 2'], ['Кнопка 3', 'Кнопка 4']]
-    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+def button(update: Update, context: CallbackContext) -> None:
+    """Обработчик нажатий на кнопки"""
+    query = update.callback_query
+    query.answer()
     
-    # Отправляем сообщение с клавиатурой
-    await update.message.reply_text('Выберите одну из кнопок:', reply_markup=reply_markup)
+    if query.data == 'pubg_key':
+        query.edit_message_text(text="Ваш ключ: 576666-336-GA3Cq")
 
-# Функция для обработки текстовых сообщений
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(f'Вы выбрали: {text}')
-
-def main():
-    # Создаем приложение и передаем токен
-    application = Application.builder().token(TOKEN).build()
+def main() -> None:
+    """Запуск бота"""
+    updater = Updater("7835506064:AAGyhtbS72oN0EyXfOI_HDNTaUtYb1pkrS8", use_context=True)
     
-    # Регистрируем обработчики команд и сообщений
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
     
-    # Запускаем бота
-    application.run_polling()
+    updater.start_polling()
+    updater.idle()
 
-if __name__ == '__main__':
+if name == 'main':
     main()
